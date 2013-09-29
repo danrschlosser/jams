7207ccea5ecfb926095e5da0fa99a1297910dd88
Dan Schlosser <dan.r.schlosser@gmail.com>
2013-09-29 00:59:56 -0400

    make the add button work

--- static/js/script.js
+++ static/js/script.js
@@ -1,65 +1,83 @@
 $(document).ready(function(){
-    jsPlumb.Defaults.Container($('#container'));
+    jsPlumb.Defaults.Container = $('#container');
     jsPlumb.draggable($('#container *'));
 
+    $('.new-clip').click(function() {
+        console.log("Test.");
+        $('#container').append(
+            $("<div>", {
+                "class": "node"
+            }).append(
+                $("<input>", {
+                    type: "text",
+                    placeholder: "Title"
+                }),
+                $("<div>", {
+                    "class": "play-pause"
+                })
+            )
+        );
+    });
+
+    
+
+    var audio_context;
+    var recorder;
+
+    var startUserMedia = function(stream) {
+        var input = audio_context.createMediaStreamSource(stream);
+        var zeroGain = audio_context.createGain();
+        zeroGain.gain.value = 0;
+        input.connect(zeroGain);
+        zeroGain.connect(audio_context.destination);
+        recorder = new Recorder(input);
+    };
+
+    var startRecording = function() {
+        recorder && recorder.record();
+        this.disabled = true;
+        this.nextElementSibling.disabled = false;
+    };
+
+    var stopRecording = function() {
+        recorder && recorder.stop();
+        this.disabled = true;
+        this.previousElementSibling.disabled = false;
+
+      
+
+        recorder.clear();
+    };
+
+    var createDownloadLink = function() {
+        recorder && recorder.exportWAV(function(blob) {
+            jQuery.ajax({
+                url: '/upload/',
+                type: 'POST',
+                data: {
+                    audio: blob
+                },
+                success: function(content){
+                }
+            });
+        });
+    };
+
+    var recordingSetup = function() {
+
+        audio_context = new webkitAudioContext()
+
+        //Compatibility
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+        var btnStart = document.getElementById("btnStart"),
+            btnStop = document.getElementById("btnStop");
+
+        navigator.getUserMedia({audio: true}, startUserMedia);
+
+        btnStart.addEventListener("click", startRecording);
+        btnStop.addEventListener('click', stopRecording);
+    };
 
     recordingSetup();
 });
-
-var audio_context;
-var recorder;
-
-function startUserMedia(stream) {
-    var input = audio_context.createMediaStreamSource(stream);
-    var zeroGain = audio_context.createGain();
-    zeroGain.gain.value = 0;
-    input.connect(zeroGain);
-    zeroGain.connect(audio_context.destination);
-    recorder = new Recorder(input);
-}
-
-function startRecording() {
-    recorder && recorder.record();
-    this.disabled = true;
-    this.nextElementSibling.disabled = false;
-}
-
-function stopRecording() {
-    recorder && recorder.stop();
-    this.disabled = true;
-    this.previousElementSibling.disabled = false;
-
-  
-
-    recorder.clear();
-}
-
-function createDownloadLink() {
-    recorder && recorder.exportWAV(function(blob) {
-    	jQuery.ajax({
-    		url: '/upload/',
-    		type: 'POST',
-    		data: {
-    			audio: blob
-    		},
-    		success: function(content){
-    		}
-    });
-}
-
-function recordingSetup() {
-
-	audio_context = new webkitAudioContext()
-
-	//Compatibility
-	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-
-	var btnStart = document.getElementById("btnStart"),
-		btnStop = document.getElementById("btnStop");
-
-	navigator.getUserMedia({audio: true}, startUserMedia);
-
-	btnStart.addEventListener("click", startRecording);
-	btnStop.addEventListener('click', stopRecording);
-}
-
