0a2b9c5579f5731ddc4a2892c0296e84b7fbfc1c
Jee Hyun Wang <jw3046@columbia.edu>
2013-09-28 22:41:57 -0400

    cleaned up javascript

--- static/js/script.js
+++ static/js/script.js
@@ -6,16 +6,15 @@ $(document).ready(function(){
     recordingSetup();
 });
 
-function onFailSoHard(e) {
-    console.log('Rejected!', e);
-};
-
 var audio_context;
 var recorder;
 
 function startUserMedia(stream) {
     var input = audio_context.createMediaStreamSource(stream);
-    input.connect(audio_context.destination);
+    var zeroGain = audio_context.createGain();
+    zeroGain.gain.value = 0;
+    input.connect(zeroGain);
+    zeroGain.connect(audio_context.destination);
     recorder = new Recorder(input);
 }
 
@@ -30,60 +29,37 @@ function stopRecording() {
     this.disabled = true;
     this.previousElementSibling.disabled = false;
 
-    // create WAV download link using audio data blob
-    createDownloadLink();
+  
 
     recorder.clear();
 }
 
 function createDownloadLink() {
     recorder && recorder.exportWAV(function(blob) {
-        var url = URL.createObjectURL(blob);
-        var recordingslist = $('#recordingslist');
-        var li = document.createElement('li');
-        var au = document.createElement('audio');
-        var hf = document.createElement('a');
-
-        au.controls = true;
-        au.src = url;
-        hf.href = url;
-        hf.download = new Date().toISOString() + '.wav';
-        hf.innerHTML = hf.download;
-        li.appendChild(au);
-        li.appendChild(hf);
-        recordingslist.append(li);
+    	jQuery.ajax({
+    		url: '/upload/',
+    		type: 'POST',
+    		data: {
+    			audio: blob
+    		},
+    		success: function(content){
+    		}
     });
 }
 
 function recordingSetup() {
-    window.onload = function() {
 
-        audio_context = new webkitAudioContext()
+	audio_context = new webkitAudioContext()
 
-        //Compatibility
-        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+	//Compatibility
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
 
-        var canvas = document.getElementById("canvas"),
-            context = canvas.getContext("2d"),
-            btnStart = document.getElementById("btnStart"),
-            btnStop = document.getElementById("btnStop"),
-            btnPhoto = document.getElementById("btnPhoto"),
-            audioObj = {
-                video: false,
-                audio: true
-            };
+	var btnStart = document.getElementById("btnStart"),
+		btnStop = document.getElementById("btnStop");
 
-        if (navigator.getUserMedia) {
-            navigator.getUserMedia(audioObj, startUserMedia);
-        }
+	navigator.getUserMedia({audio: true}, startUserMedia);
 
-        btnStart.addEventListener("click", startRecording);
-        btnStop.addEventListener('click', stopRecording);
-    }
+	btnStart.addEventListener("click", startRecording);
+	btnStop.addEventListener('click', stopRecording);
 }
 
-function hasGetUserMedia() {
-    // Note: Opera is unprefixed.
-    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia || navigator.msGetUserMedia);
-}
