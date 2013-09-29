fbbe5aaf6a74f099fac929acf6f14a4d8aa35b8d
Dan Schlosser <dan.r.schlosser@gmail.com>
2013-09-29 06:17:05 -0400

    finish first version of the frontend

--- static/js/script.js
+++ static/js/script.js
@@ -1,177 +1,145 @@
 $(document).ready(function(){
+    // setup jsPlumb defaults.
     jsPlumb.importDefaults({
-        DragOptions : { cursor: "pointer", zIndex:2000 },
-        HoverClass:"connector-hover"
+        HoverClass : "dropHover", 
+        DragOptions : { cursor: 'pointer', zIndex:2000 },
+        PaintStyle : { strokeStyle:'#666' },
+        EndpointStyle : { width:20, height:16, strokeStyle:'#666' },
+        Endpoint : "Rectangle",
+        Anchors : ["Left", "Right"],
+        Container : "container"
     });
 
-    recordingSetup();
-
-    jsPlumb.Defaults.Container = $('#container');
-    jsPlumb.draggable(jsPlumb.getSelector(".node"));
-
-    var index = 2;
-    var previous = "node1";
+    // jsPlumb.Defaults.Container = $('#container');
+    
+    var index = 1;
     $('.new-node').click(function(e) {
-        e.preventDefault();
-        var id = "node" + index;
-        console.log("Test.");
-        $('#container').append(
+        $(".greeting").fadeTo(200,0);
+        e.preventDefault(index);
+        index += 1;
+    });                                             
+
+    // bind to connection/connectionDetached events, and update the list of connections on screen.
+    jsPlumb.bind("connection", function(info, originalEvent) {
+        updateConnections(info.connection);
+        console.log("connected");
+    });
+    
+    jsPlumb.bind("connectionDetached", function(info, originalEvent) {
+        updateConnections(info.connection, true);
+        console.log("disconnected")
+    });
+
+    var audio_context;
+    var recorder;
+
+    // recordingSetup();
+});
+
+var createNode = function(index) {
+    var id = "node" + index;
+    console.log("Test.");
+    $('#container').append(
+        $("<div>", {
+            "class": "node",
+            id: id
+        }).append(
+            $("<input>", {
+                type: "text",
+                placeholder: "Title"
+            }),
             $("<div>", {
-                "class": "node",
-                id: id
+                "class": "size"
             }).append(
-                $("<input>", {
-                    type: "text",
-                    placeholder: "Title"
-                }),
-                $("<div>", {
-                    "class": "size"
+                $("<a>", {
+                    href: "#"
                 }).append(
-                    $("<a>", {
-                        href: "#"
-                    }).append(
-                        $("<div>", {
-                            "class": "play-node icon-play"
-                        })
-                    )
+                    $("<div>", {
+                        "class": "play-node icon-play"
+                    })
                 )
             )
-        );
-        jsPlumb.draggable(id);
+        )
+    );
+    jsPlumb.draggable(id);
 
-        var common = {
-            cssClass:"myCssClass"
-        };
-        jsPlumb.connect({
-            source: previous,
-            target: id,
-            anchor:[ "Continuous", { faces:["top","bottom"] }],
-            endpoint:[ "Dot", { radius:5, hoverClass:"myEndpointHover" }, common ],
-            connector:[ "Bezier", { curviness:100 }, common ],
-            overlays: [
-                [ "Arrow", { foldback:0.2 }, common ]    
-            ]
+    // configure some drop options for use by all endpoints.
+    var dropOptions = {
+        tolerance:"touch",
+        hoverClass:"dropHover",
+        activeClass:"dragActive"
+    };
+
+    var color2 = "#316b31";
+    var endpoint = {
+        endpoint:["Dot", { radius:11 }],
+        paintStyle:{ fillStyle:color2 },
+        isSource:true,
+        scope:"green dot",
+        connectorStyle:{ strokeStyle:color2, lineWidth:6 },
+        connector: ["Flowchart", { cornerRadius:10 } ],
+        isTarget:true,
+        maxConnections: 10,
+        dropOptions : dropOptions
+    };
+    var e1 = jsPlumb.addEndpoint(id, { anchor: [1, 0.5, 1, 0]}, endpoint);
+    var e2 = jsPlumb.addEndpoint(id, { anchor: [0, 0.5, -1, 0]}, endpoint)
+}
+
+
+var blink = function(){
+    console.log("blinkin'");
+    $('.icon-circle-empty').delay(200).fadeTo(200,0.5).delay(200).fadeTo(200,1, blink);
+}
+
+var startUserMedia = function(stream) {
+    var input = audio_context.createMediaStreamSource(stream);
+    var zeroGain = audio_context.createGain();
+    zeroGain.gain.value = 0;
+    input.connect(zeroGain);
+    zeroGain.connect(audio_context.destination);
+    recorder = new Recorder(input);
+};
+
+var startRecording = function() {
+    recorder && recorder.record();
+    this.disabled = true;
+    this.nextElementSibling.disabled = false;
+};
+
+var stopRecording = function() {
+    recorder && recorder.stop();
+    this.disabled = true;
+    this.previousElementSibling.disabled = false;
+
+  
+
+    recorder.clear();
+};
+
+var createDownloadLink = function() {
+    recorder && recorder.exportWAV(function(blob) {
+        jQuery.ajax({
+            url: '/upload/',
+            type: 'POST',
+            data: {
+                audio: blob
+            },
+            success: function(content){
+            }
         });
-
-        previous = id;
-        index = index + 1;
     });
+};
 
-    var audio_context;
-    var recorder;
+var recordingSetup = function() {
 
-    function startUserMedia(stream) {
-        var input = audio_context.createMediaStreamSource(stream);
-        var zeroGain = audio_context.createGain();
-        zeroGain.gain.value = 0;
-        input.connect(zeroGain);
-        zeroGain.connect(audio_context.destination);
-        recorder = new Recorder(input);
-    };
+    audio_context = new webkitAudioContext()
 
-    function startRecording() {
-        recorder && recorder.record();
-        this.disabled = true;
-        this.nextElementSibling.disabled = false;
-    };
+    //Compatibility
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
 
-    function stopRecording() {
-        recorder && recorder.stop();
-        this.disabled = true;
-        this.previousElementSibling.disabled = false;
-
-        recorder && recorder.exportWAV(function(blob) {
-    /*        console.log(blob);
-            $('userfile').val(blob);
-            $.ajaxFileUpload({
-                url: "../ajax/saverecording/", 
-                secureuri: false,
-                fileElementId: 'userfile',
-                dataType: blob.type,
-                data: blob,
-                success: function(data, status) {
-                    if(data.status != 'error')
-                        alert("hoera!");
-                    alert(data.msg);
-                }
-            });
-            */
-
-            var fd = new FormData();
-            fd.append('audio', blob);
-            $.ajax({
-                url: '/upload',
-                type: 'POST',
-                data: fd,
-                processData: false,
-                contentType: false
-            }).done(function(data) {
-                console.log(data);
-            });
-        });
-        recorder.clear();
-    };
-
-    var blink = function(){
-        console.log("blinkin'");
-        $('.icon-circle-empty').delay(200).fadeTo(200,0.5).delay(200).fadeTo(200,1, blink);
-    }
-    blink();
-
-    var audio_context;
-    var recorder;
-
-    var startUserMedia = function(stream) {
-        var input = audio_context.createMediaStreamSource(stream);
-        var zeroGain = audio_context.createGain();
-        zeroGain.gain.value = 0;
-        input.connect(zeroGain);
-        zeroGain.connect(audio_context.destination);
-        recorder = new Recorder(input);
-    };
-
-    var startRecording = function() {
-        recorder && recorder.record();
-        this.disabled = true;
-        this.nextElementSibling.disabled = false;
-    };
-
-    var stopRecording = function() {
-        recorder && recorder.stop();
-        this.disabled = true;
-        this.previousElementSibling.disabled = false;
-
-      
-
-        recorder.clear();
-    };
-
-    var createDownloadLink = function() {
-        recorder && recorder.exportWAV(function(blob) {
-            jQuery.ajax({
-                url: '/upload/',
-                type: 'POST',
-                data: {
-                    audio: blob
-                },
-                success: function(content){
-                }
-            });
-        });
-    };
-
-    var recordingSetup = function() {
-
-        audio_context = new webkitAudioContext()
-
-        //Compatibility
-        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-
-        var btnStart = document.getElementById("btnStart"),
-            btnStop = document.getElementById("btnStop");
-
-        navigator.getUserMedia({audio: true}, startUserMedia);
-
+    var btnStart = document.getElementById("btnStart"),
+        btnStop = document.getElementById("btnStop");
         btnStart.addEventListener("click", startRecording);
         btnStop.addEventListener('click', stopRecording);
     };
